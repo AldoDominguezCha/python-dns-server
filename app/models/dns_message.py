d5339ee997405fce974e5edf7f46f8d909709fc0
Aldo Dominguez <aldodominguezchz@gmail.com>
2024-08-29 17:13:06 -0600

    remove models folder temporarily

--- app/models/dns_message.py
+++
@@ -1,9 +0,0 @@
-class DNSMessage:
-    def __init__(self):
-        self.header: DNSMessageHeader = DNSMessageHeader()
-
-    def get_message_bytes(self) -> bytes:
-        message_bytes = b''
-        message_bytes += self.header.get_header_bytes()
-
-        return message_bytes
