b89e0321f48c2ff409d471e405aae4571ea55365
Aldo Dominguez <aldodominguezchz@gmail.com>
2024-08-30 17:19:20 -0600

    refactor to use writer/parser classes

--- app/main.py
+++ app/main.py
@@ -1,188 +1,188 @@
+from __future__ import annotations
 import socket
 
-'''
-Using this decorator we are extending the parameter class, we are generating a new subclass with an additional method
-to get the label sequence as bytes. This is unnecessary in reality, as simple inheritance would have sufficed using 
-LabelSequenceEncoder as the base class, but it's an interesting example about applying decorators to a class instead of
-a function.
-'''
-def label_sequence(cls):
-    class LabelSequenceEncoder(cls):
-        def __init__(self, *args, **kargs):
-            super().__init__(*args, **kargs)
+# Manipulate a message, to set the data into it so we can send it as a byte sequence at the end
+class DNSMessageWriter(object):
+    @staticmethod
+    def __encode(number: int, number_of_bytes: int) -> bytes:
+        # Get the sequence of bytes representing the number in big-endian format as required by the DNS specification
+        return (number).to_bytes(number_of_bytes, byteorder='big')
 
-        def get_label_sequence_bytes(self, domain_name: str):
-            label_sequence = b''
+    # Get the bytes representing the domain name (it will be encoded as a sequence of labels)
+    @staticmethod
+    def __encode_label_sequence(name: str) -> bytes:
+        label_sequence = b''
 
-            name_segments = domain_name.split('.')
-            for segment in name_segments:
-                label_sequence += len(segment).to_bytes(1, byteorder='big') + segment.encode('UTF-8')
-            label_sequence += b'\x00'
+        for segment in name.split('.'):
+            label_sequence += DNSMessageWriter.__encode(len(segment), 1) + segment.encode('UTF-8')
+        label_sequence += b'\x00'
 
-            return label_sequence
-
-    return LabelSequenceEncoder
-
-
-class DNSMessage:
-    def __init__(self):
-        self.header: DNSMessageHeader = DNSMessageHeader()
-        # It's a list of questions
-        self.questions = []
-        # It's a list of answer records to the questions
-        self.answers = []
+        return label_sequence
     
-    def add_message_question(self, dns_question):
-        self.questions.append(dns_question)
-        self.header.increment_question_count()
-
-    def add_message_answer(self, dns_record):
-        self.answers.append(dns_record)
-        self.header.increment_answer_count()
-    
-    def get_message_bytes(self) -> bytes:
-        message_bytes = b''
-        message_bytes += self.header.get_header_bytes()
-        message_bytes += b''.join([question.question_bytes for question in self.questions])
-        message_bytes += b''.join([record.get_record_bytes() for record in self.answers])
-
-        return message_bytes
-
-class DNSMessageHeader:
-    """Abstraction of the DNS message header"""
-
-    '''
-        This is the constructor mmethod of a class in Python.
-        When a class defines an __init__ method, class instantiation automatically invokes __init__()
-        for the newly created object, meaning that with __init__ we customize our newly instantiated
-        object's intial state.
-    '''
-    def __init__(self):
-        # These are instance variables since they are defined inside the constructor of the class,
-        # instance variables are for data that is unique to each class instance.
-        self.packet_id = 1234
-        # Indicates if the DNS message is a query or a response to a query (1 is for response).
-        self.query_or_response_indicator = 1
-        self.operation_code = 0
-        # Specifies if the responding server "owns" the domain being queried, i.e., it's authoritative (1 if it is).
-        self.authoritative_answer = 0
-        # Indicates if the message is larger than 512 bytes. Always 0 in UDP responses and we'll be using a UDP socket for the server.
-        self.truncation = 0
-        # When the message is a query, this parameter indicates if the server should recursively resolve this query.
-        self.recursion_desired = 0
-        # The resolving server sets this value to 1 if recursion is available.
-        self.recursion_available = 0
-        # Used by DNSSEC queries. At inception, it was reserved for future use.
-        self.reserved = 0
-        # Status of the response (when the DNS message is a response), 0 means no error.
-        self.response_code = 0
-        # Number of questions present in the question section of the DNS message.
-        self.question_count = 0
-        # Number of records present in the answer section of the DNS message.
-        self.answer_record_count = 0
-        # Number of records in the authority section of the DNS message.
-        self.authority_record_count = 0
-        # Number of records in the additional section of the DNS message.
-        self.additional_record_count = 0
-    
-    def increment_question_count(self):
-        self.question_count += 1
-
-    def increment_answer_count(self):
-        self.answer_record_count += 1
-
-    def get_header_bytes(self) -> bytes:
+    @staticmethod
+    def get_header_bytes(header: DNSMessageHeader) -> bytes:
         header_bytes: bytes = b''
-        # Get the bytes representation of the packet ID using two bytes (first parameter) as specified in the DNS protocol, encode
-        # this number using the big-endian format (second parameter)
-        header_bytes += self.packet_id.to_bytes(2, byteorder='big')
-        # We need to form the next byte in the header composed of the following flags and values: QR, OPCODE, AA, TC, AND RD
-        next_byte_as_binary_string = str(self.query_or_response_indicator) + '{0:04b}'.format(self.operation_code) + str(self.authoritative_answer) + str(self.truncation) + str(self.recursion_desired)
-        header_bytes += int(next_byte_as_binary_string, 2).to_bytes(1, byteorder='big')
-        # Once again, form the next byte that is comprised of multiple flags and values: RA, Z, RCODE
-        next_byte_as_binary_string = str(self.recursion_available) + '{0:03b}'.format(self.reserved) + '{0:04b}'.format(self.response_code)
-        header_bytes += int(next_byte_as_binary_string, 2).to_bytes(1, byteorder='big')
-        header_bytes += self.question_count.to_bytes(2, byteorder='big')
-        header_bytes += self.answer_record_count.to_bytes(2, byteorder='big')
-        header_bytes += self.authority_record_count.to_bytes(2, byteorder='big')
-        header_bytes += self.additional_record_count.to_bytes(2, byteorder='big')
+
+        header_bytes += DNSMessageWriter.__encode(header.packet_id, 2)
+
+        # Encode this next byte composed by multiple header fields
+        next_byte_as_binary_string = str(header.query_or_response_indicator) + '{0:04b}'.format(header.operation_code) + str(header.authoritative_answer) + str(header.truncation) + str(header.recursion_desired)
+        header_bytes += DNSMessageWriter.__encode(int(next_byte_as_binary_string, 2), 1)
+
+        # Encode this next byte composed by multiple header fields
+        next_byte_as_binary_string = str(header.recursion_available) + '{0:03b}'.format(header.reserved) + '{0:04b}'.format(header.response_code)
+        header_bytes += DNSMessageWriter.__encode(int(next_byte_as_binary_string, 2), 1)
+
+        header_bytes += DNSMessageWriter.__encode(header.question_count, 2)
+        header_bytes += DNSMessageWriter.__encode(header.answer_record_count, 2)
+        header_bytes += DNSMessageWriter.__encode(header.authority_record_count, 2)
+        header_bytes += DNSMessageWriter.__encode(header.additional_record_count, 2)
 
         print(f'Message header bytes encoded: {header_bytes}')
 
         # Sequence of bytes can be appended with one another, that's just what we did, grouping together the different header properties that conform a single byte
         # b'\x04' + b'\xd2' = b'\x04\xd2'
         return header_bytes
-
-@label_sequence
-class DNSQuestion:
-    def __init__(self, domain_name, record_type, question_class):
-        self.__domain_name: str = domain_name
-        self.__record_type: int = record_type
-        self.__question_class: int = question_class
-
-    def set_domain_name(self, readable_domain_name: str):
-        self.__domain_name = readable_domain_name
-    def set_type(self, record_type: int):
-        self.__record_type = record_type
-    def set_question_class(self, question_class: int):
-        self.__question_class = question_class
     
-    @property
-    def question_bytes(self) -> bytes:
-        question = b''
-        question_name = self.get_label_sequence_bytes(self.__domain_name)
-        question += question_name + self.__record_type.to_bytes(2, byteorder='big') + self.__question_class.to_bytes(2, byteorder='big')
+    @staticmethod
+    def get_question_bytes(question: DNSQuestion) -> bytes:
+        question_name = DNSMessageWriter.__encode_label_sequence(self.__domain_name)
+        question_bytes: bytes = question_name + DNSMessageWriter.__encode(question.record_type, 2) + DNSMessageWriter.__encode(question.question_class, 2)
         
         return question
-
-class DNSRecord:
-    def __init__(self, preamble, ip_address: str):
-        self.preamble = preamble
-        self.ip = ip_address
-    
-    def get_record_bytes(self):
-        encoded_ip = b''.join(int(ip_byte).to_bytes(1, byteorder='big') for ip_byte in self.ip.split('.'))
-        print(f'Encoded IP obtained in DNS record: {encoded_ip}')
-
-        return self.preamble.preamble_bytes + encoded_ip
-
-
-@label_sequence
-class DNSRecordPreamble:
-    def __init__(self, domain_name, record_type, record_class, time_to_live, data_length):
-        self.__domain_name: str = domain_name
-        self.__record_type: int = record_type
-        self.__record_class = record_class
-        self.__TTL = time_to_live
-        self.__data_length = data_length
-
-    @property
-    def preamble_bytes(self):
-        preamble_bytes: bytes = self.get_label_sequence_bytes(self.__domain_name)
-        preamble_bytes += self.__record_type.to_bytes(2, byteorder='big')
-        preamble_bytes += self.__record_class.to_bytes(2, byteorder='big')
-        preamble_bytes += self.__TTL.to_bytes(4, byteorder='big')
-        preamble_bytes += self.__data_length.to_bytes(2, byteorder='big')
+        
+    @staticmethod
+    def get_preamble_bytes(preamble: DNSRecordPreamble) -> bytes:
+        preamble_bytes: bytes = DNSMessageWriter.__encode_label_sequence(preamble.domain_name)
+        preamble_bytes += DNSMessageWriter.__encode(preamble.record_type, 2)
+        preamble_bytes += DNSMessageWriter.__encode(preamble.record_class, 2)
+        preamble_bytes += DNSMessageWriter.__encode(preamble.TTL, 4)
+        preamble_bytes += DNSMessageWriter.__encode(preamble.data_length, 2)
 
         return preamble_bytes
 
+    @staticmethod
+    def get_record_bytes(record: DNSRecord):
+        encoded_ip = b''.join(DNSMessageWriter.__encode(int(ip_byte), 1) for ip_byte in record.ip.split('.'))
+        print(f'Encoded IP obtained in DNS record: {encoded_ip}')
 
+        return record.preamble.preamble_bytes + encoded_ip
+    
+    
+    @staticmethod
+    def encode_message(dns_message: DNSMessage) -> bytes:
+        header_bytes: bytes = DNSMessageWriter.get_header_bytes(dns_message.header)
+
+        message_bytes = b''
+        message_bytes += self.header.get_header_bytes()
+        message_bytes += b''.join([DNSMessageWriter.get_question_bytes(question) for question in dns_message.questions])
+        message_bytes += b''.join([DNSMessageWriter.get_record_bytes(record) for record in dns_message.answers])
+
+        return message_bytes
+
+# Receive a raw DNS message (the sequence of bytes), extract the sections and the parts of each section,
+# and parse the data to make it human-readable.
+class DNSMessageParser(object):
+    def __init__(self, raw_dns_message: bytes):
+        self.__raw_message = raw_dns_message
+
+
+
+class DNSMessage:
+    def __init__(self, packet_id: int = 0, query_response: int = 0):
+        self.__header: DNSMessageHeader = DNSMessageHeader(packet_id, query_response)
+        # TODO: Make them private with a getter
+        self.questions = []
+        self.answers = []
+
+    @property
+    def header(self):
+        return self.__header
+    
+    def add_message_question(self, dns_question):
+        self.questions.append(dns_question)
+        self.__header.increment_question_count()
+
+    def add_message_answer(self, dns_record):
+        self.answers.append(dns_record)
+        self.__header.increment_answer_count()
+        
+
+class DNSMessageHeader:
+    def __init__(self, packet_id: int = 0, query_response: int = 0):
+        self.packet_id = packet_id
+        self.query_or_response_indicator = query_response
+        self.operation_code = 0
+        self.authoritative_answer = 0
+        self.truncation = 0
+        self.recursion_desired = 0
+        self.recursion_available = 0
+        self.reserved = 0
+        self.response_code = 0
+        self.__question_count = 0
+        self.__answer_record_count = 0
+        self.__authority_record_count = 0
+        self.__additional_record_count = 0
+
+    @property
+    def question_count(self):
+        return self.__question_count
+
+    @property
+    def answer_record_count(self):
+        return self.__answer_record_count
+
+    @property
+    def authority_record_count(self):
+        return self.__authority_record_count
+
+    @property
+    def additional_record_count(self):
+        return self.__additional_record_count
+    
+    def increment_question_count(self):
+        self.question_count += 1
+
+    def increment_answer_count(self):
+        self.answer_record_count += 1
+        
+class DNSQuestion:
+    def __init__(self, domain_name, record_type, question_class):
+        self.domain_name: str = domain_name
+        self.record_type: int = record_type
+        self.question_class: int = question_class
+        
+
+class DNSRecord:
+    def __init__(self, preamble: DNSRecordPreamble, ip_address: str):
+        self.preamble = preamble
+        self.ip = ip_address
+        
+
+class DNSRecordPreamble:
+    def __init__(self, domain_name, record_type, record_class, time_to_live, data_length):
+        self.domain_name: str = domain_name
+        self.record_type: int = record_type
+        self.record_class = record_class
+        self.TTL = time_to_live
+        self.data_length = data_length
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
     
+    # TODO: Replace this with a ThreadPoolExecutor
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
             # Build DNS response message
             # TODO: Create a writer class for this
-            dns_message = DNSMessage()
-            dns_message.add_message_question(DNSQuestion('codecrafters.io', 1, 1))
-            dns_message.add_message_answer(DNSRecord(DNSRecordPreamble('codecrafters.io', 1, 1, 60, 4), '8.8.8.8'))
+            dns_response_message = DNSMessage(1234, 1)
+            dns_response_message.add_message_question(DNSQuestion('codecrafters.io', 1, 1))
+            dns_response_message.add_message_answer(DNSRecord(DNSRecordPreamble('codecrafters.io', 1, 1, 60, 4), '8.8.8.8'))
 
 
-            response: bytes = dns_message.get_message_bytes()
+            response: bytes = DNSMessageWriter.encode_message(dns_response_message)
     
             udp_socket.sendto(response, source)
         except Exception as e:
