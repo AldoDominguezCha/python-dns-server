0ad8d6662bf1ee34c8ed38c0f001b7feb1179184
Aldo Dominguez <aldodominguezchz@gmail.com>
2024-09-03 21:07:15 -0600

    parse answer records section

--- app/main.py
+++ app/main.py
@@ -92,7 +92,8 @@ class DNSMessageParser(object):
         self.__message = DNSMessage()
 
         self.parse_header()
-        self.parse_questions(self.message.header.question_count)
+        pointer = self.parse_questions(self.message.header.question_count)
+        self.parse_answer_records(pointer)
     
     @property
     def message(self):
@@ -154,7 +155,6 @@ class DNSMessageParser(object):
 
     def parse_question(self, starting_point_in_bytes: int):
         pointer, domain_name_slices = starting_point_in_bytes, []
-        current_label_byte = self.__raw_message[pointer : pointer + 1]
 
         while True:
             current_label_byte = self.__raw_message[pointer : pointer + 1]
@@ -172,7 +172,7 @@ class DNSMessageParser(object):
                 offset  = format(int.from_bytes(self.__raw_message[pointer : pointer + 2]), '016b')
                 # We remove the MSB of the two bytes that conform the pointer, these are just flags, not actually part of the pointer
                 offset = '00' + offset[2:]
-                question, = self.parse_question(int(offset, base=2))
+                question, _ = self.parse_question(int(offset, base=2))
                 pointer_after_offset = pointer + 2
 
                 # At this pointe we found a pointer to a compressed label sequence (the pointer to a label sequence that has appeared already), we've parsed it
@@ -207,9 +207,85 @@ class DNSMessageParser(object):
 
         while question_count > 0:
             question, pointer = self.parse_question(pointer)
-            print('Parsed question domain name: ' + question.domain_name)
             self.message.questions.append(question)
             question_count -= 1
+
+        return pointer
+    
+    def parse_answer_record(self, starting_point_in_bytes: int):
+        pointer, domain_name_slices = starting_point_in_bytes, []
+
+        while True:
+            current_label_byte = self.__raw_message[pointer : pointer + 1]
+
+            # This null byte as the label length indicates that the current label sequence ends
+            if current_label_byte == b'\x00':
+                pointer += 1
+                break
+
+            label_length_as_bits = format(int.from_bytes(current_label_byte), '08b')
+
+            # This means this is not a regular label sequence, but a pointer to a label that has appeared before in the message (instead of reapeating the domain name
+            # we point to a previous occurrence)
+            if label_length_as_bits[0:2] == '11':
+                offset  = format(int.from_bytes(self.__raw_message[pointer : pointer + 2]), '016b')
+                # We remove the MSB of the two bytes that conform the pointer, these are just flags, not actually part of the pointer
+                offset = '00' + offset[2:]
+                answer, _ = self.parse_question(int(offset, base=2))
+                pointer_after_offset = pointer + 2
+
+                # At this pointe we found a pointer to a compressed label sequence (the pointer to a label sequence that has appeared already), we've parsed it
+                # but we still need to append at the beginning all the labels we found for the current question before we found the compression pointer,
+                # we could have something like {{ REGULAR_LABEL }}.{{ COMPRESSION_POINTER }}
+                answer.preamble.domain_name = '.'.join(domain_name_slices) + question.domain_name
+
+                return (answer, pointer_after_offset)
+
+            else:
+                # This means this is still a regular label sequence, move the pointer past the current label length byte
+                pointer += 1
+                # The the next label length (in bytes) from the variable that still contains the next label length (the pointer was moved already but this
+                # variable still has the value)
+                label_size = int.from_bytes(current_label_byte)
+                # Get the next label in the sequence and append it to the domain name slices
+                label = self.__raw_message[pointer : pointer + label_size].decode('UTF-8')
+                pointer += label_size
+                domain_name_slices.append(label)
+
+            
+        domain_name = '.'.join(domain_name_slices)
+        record_type = int.from_bytes(self.__raw_message[pointer : pointer + 2])
+        pointer += 2
+        record_class = int.from_bytes(self.__raw_message[pointer : pointer + 2])
+        pointer += 2
+        time_to_live = int.from_bytes(self.__raw_message[pointer : pointer + 4])
+        pointer += 4
+        data_length = int.from_bytes(self.__raw_message[pointer : pointer + 2])
+        pointer += 2
+
+        # Parse the IP in the answer record, the IP is encoded as a four bytes
+        ip_numbers = []
+        for i in range(4):
+            next_ip_number = int.from_bytes(self.__raw_message[pointer : pointer + 1])
+            ip_numbers.append(str(next_ip_number))
+
+            pointer += 1
+
+        ip_address = '.'.join(ip_numbers)
+
+
+        return (DNSRecord(DNSRecordPreamble(domain_name, record_type, record_class, time_to_live, data_length), ip_address), pointer)
+    
+    def parse_answer_records(self, starting_point_in_bytes: int, answer_count: int):
+        pointer = starting_point_in_bytes
+
+        while answer_count:
+            answer, pointer = self.parse_answer_record(pointer)
+            print(f'In parsed answer record. Domain name: {answer.preamble.domain_name}')
+            print(f'In parsed answer record. IP: {answer.ip}')
+            self.message.answers.append(answer)
+            answer_count -= 1
+
 
 
 class DNSMessage:
@@ -317,6 +393,8 @@ def handle_dns_query(server_udp_socket, buffer: bytes, source, resolver):
         # Forward query
         server_udp_socket.sendto(DNSMessageEncoder.encode_message(message), resolver_address)
         raw_forward_response, _ = server_udp_socket.recvfrom(512)
+
+        new_parser = DNSMessageParser(raw_forward_response)
 
         print(f'Raw response form forward server: {raw_forward_response}')
 
