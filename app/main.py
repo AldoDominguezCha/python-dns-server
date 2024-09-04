01397f7fc2b643d44f4c2dcb6b5d7a909385d148
Aldo Dominguez <aldodominguezchz@gmail.com>
2024-09-03 22:24:52 -0600

    add log

--- app/main.py
+++ app/main.py
@@ -389,7 +389,7 @@ def handle_dns_query(server_udp_socket, buffer: bytes, source, resolver):
 
             forward_response_parser = DNSMessageParser(raw_forward_response)
             print(f'Length of answers: {len(forward_response_parser.message.answers)}')
-            print(f'Raw answer bytes from forward server: {forward_response_parser.message.raw_answer_bytes}')
+            print(f'Raw answer bytes from forward server: {forward_response_parser.raw_answer_bytes}')
             if forward_response_parser.message.answers:
                 print(f'From forward server, maion name: {forward_response_parser.message.answers[0].preamble.domain_name}')
                 original_message.add_message_answer(forward_response_parser.message.answers[0])
