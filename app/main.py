8fa6bc7307846619e40d4f7deabacafe97af9eb0
Aldo Dominguez <aldodominguezchz@gmail.com>
2024-09-06 20:51:46 -0600

    test

--- app/main.py
+++ app/main.py
@@ -383,7 +383,7 @@ def handle_dns_query(server_udp_socket, buffer: bytes, source, resolver):
         for question in original_message.questions:
             print('INSIDE FOR LOOP')
             forward_query_message = DNSMessage(original_message.header.packet_id)
-            forward_query_message.header.recursion_desired = 1
+            forward_query_message.header.recursion_desired = 0
             forward_query_message.add_message_question(question)
 
             print(f'Question count in forward message: {forward_query_message.header.question_count}')
