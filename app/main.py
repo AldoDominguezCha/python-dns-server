6689745d99752a92fbd89509925724dc9a29d4fe
Aldo Dominguez <aldodominguezchz@gmail.com>
2024-09-09 16:30:09 -0600

    add test

--- app/main.py
+++ app/main.py
@@ -380,7 +380,7 @@ def handle_dns_query(server_udp_socket, buffer: bytes, source, resolver):
         original_message.header.response_code = 0 if not original_message.header.operation_code else 4
 
         if len(original_message.questions) == 2:
-            original_message.questions[1].domain_name = 'abc.' + original_message.questions[1].domain_name
+            original_message.questions[1].domain_name = original_message.questions[0].domain_name
 
 
 
